9d29021274b8d13bd9322964db2a0e789fa18f6a
Jonathan Spearman <jonathan.spearman@gmail.com>
2026-09-07 19:12:02 +0000

    Add copyright notices and LICENSE (all rights reserved); bump Vitamin Village cache to v2

--- vitamin-village/sw.js
+++ vitamin-village/sw.js
@@ -8,7 +8,7 @@
    phone knows to throw away the old copy and fetch your new one. If your edits
    "don't show up" on a phone, this is almost always why.
 */
-const CACHE_NAME = "vitamin-village-v1";
+const CACHE_NAME = "vitamin-village-v2";
 const FILES = [
   "./",
   "./index.html",
