e9f6856350fd4c39eed3c024c746fba4c2222019
1084 <jonathan.spearman@gmail.com>
2026-09-12 18:54:23 -0400

    Vitamin Village v6: bump service worker cache to v6

--- vitamin-village/sw.js
+++ vitamin-village/sw.js
@@ -8,7 +8,7 @@
    phone knows to throw away the old copy and fetch your new one. If your edits
    "don't show up" on a phone, this is almost always why.
 */
-const CACHE_NAME = "vitamin-village-v4";
+const CACHE_NAME = "vitamin-village-v6";
 const FILES = [
   "./",
   "./index.html",
